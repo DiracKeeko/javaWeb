5a15c6021844148c73e5e9d5e1f20fdaedd021e8
Keeko <787018661@qq.com>
2023-02-17 17:26:13 +0800

    [feat] getServletContext().getResourceAsStream() 读取配置

--- javaweb-02-servlet/servlet-02/src/main/java/com/keeko/servlet/ServletDemo05Properties.java
+++ javaweb-02-servlet/servlet-02/src/main/java/com/keeko/servlet/ServletDemo05Properties.java
@@ -1,4 +1,32 @@
 package com.keeko.servlet;
 
-public class ServletDemo05Properties {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ServletDemo05Properties extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        // "/WEB-INF/classes/db.properties" 必须以"/"开头, 表示项目根目录
+        InputStream is = this.getServletContext().getResourceAsStream("/WEB-INF/classes/db.properties");
+
+        // InputStream is2 = this.getServletContext().getResourceAsStream("/WEB-INF/classes/com/keeko/servlet/aa.properties");
+
+        Properties prop = new Properties();
+        prop.load(is);
+        String user = prop.getProperty("username");
+        String pwd = prop.getProperty("password");
+
+        resp.getWriter().print(user+":"+pwd);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
