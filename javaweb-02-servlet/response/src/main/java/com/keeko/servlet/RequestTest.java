1bf1034d22ff447420e0cb516792a4749ae34924
Keeko <787018661@qq.com>
2023-02-27 17:15:32 +0800

    [chore] Request的常用方法

---
+++ javaweb-02-servlet/response/src/main/java/com/keeko/servlet/RequestTest.java
@@ -0,0 +1,33 @@
+package com.keeko.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RequestTest extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 处理请求
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+
+        /*
+            req.getParameter() 返回字符串
+            req.getParameterValues()  返回字符串数组
+        * */
+
+        System.out.println(username+":"+password);
+
+        // 重定向时需注意路径问题，否则为404;
+        // /response/success.jsp
+        resp.sendRedirect("/response/success.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
