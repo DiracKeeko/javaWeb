382d3b2a25f9598189112718bc27e728d42bf736
Keeko <787018661@qq.com>
2023-03-07 16:28:54 +0800

    [feat] 在session中存储对象

--- javaweb-session-cookie/src/main/java/com/keeko/servlet/SessionDemo02.java
+++ javaweb-session-cookie/src/main/java/com/keeko/servlet/SessionDemo02.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-public class SessionDemo01 extends HttpServlet {
+public class SessionDemo02 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // 解决乱码问题
@@ -19,17 +19,11 @@ public class SessionDemo01 extends HttpServlet {
 
         // 得到Session
         HttpSession session = req.getSession();
-        // 在session中存东西
-        session.setAttribute("name", new Person("小明", 16));
-        // 获取Session中的ID
-        String id = session.getId();
 
-        // 判断Session是否为新创建的
-        if(session.isNew()){
-            resp.getWriter().write("session创建成功！ID:" + id);
-        }else{
-            resp.getWriter().write("session已经在服务器中存在了！ID:" + id);
-        }
+        Person p = (Person) session.getAttribute("name");
+
+        System.out.println("name in session->" + p.toString());
+
     }
 
     @Override
